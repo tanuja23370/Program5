815c052f655d1a377718dc331b80e10c273364a8
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-06 11:52:39 -0300

    save

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+package Fibonacci;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int count = 10; // You can modify this value as needed
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series up to " + count + " terms:");
+        for (int i = 1; i <= count; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
